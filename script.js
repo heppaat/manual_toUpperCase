0d7ece7ae512c39cc475ffb07ea860995d6f1940
heppaat <pali.herrer@gmail.com>
2023-11-12 22:01:13 +0100

    tested some callback functions

---
+++ script.js
@@ -0,0 +1,25 @@
+let geeks_2 = [
+  { username: "Pali", password: "123456" },
+  { username: "Gergo", password: "1234" },
+  { username: "Soma", password: "12345" },
+  { username: "Pisti", password: "123" },
+];
+
+function newPassword(list) {
+  if (list.password.length < 5) {
+    return true;
+  }
+  return false;
+}
+
+function MyFilter(arr, filterFunction) {
+  let result = [];
+  for (let i = 0; arr[i] !== undefined; i++) {
+    if (filterFunction(arr[i])) {
+      result = [...result, arr[i]];
+    }
+  }
+  return result;
+}
+let result_1 = MyFilter(geeks_2, newPassword);
+console.log(result_1);
